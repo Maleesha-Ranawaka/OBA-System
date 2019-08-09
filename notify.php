78b03c7f17ffde0e36ad9106346e5f1527b3cb40
ANGEL-141 <umranawaka@gmail.com>
2019-08-10 03:33:44 +0530

    edits on notify,php

--- notify.php
+++ notify.php
@@ -8,8 +8,29 @@ $status_code         = $_POST['status_code'];
 
 if ($status_code == 2 ){
         //TODO: Update  database as payment success
-       
 
+        require_once 'php/conn.php';
+
+        $regestration_number = $order_id;
+
+        $query = "UPDATE user SET status='2' WHERE regestration_number='$regestration_number'";
+
+        if ($conn->query($query) === TRUE) {
+                echo "Record ".$regestration_number." updated successfully";
+            } else {
+                echo "Error - ".$regestration_number." updating record: " . $conn->error;
+            }
+
+        $query1 = "INSERT INTO payment (timestamp, type, regestration_number) VALUES (now(), 'Annual', $regestration_number)";
+
+        if ($conn->query($query1) === TRUE) {
+                echo "<br> Record ".$regestration_number." inserted successfully";
+            } else {
+                echo "<br> Error - ".$regestration_number." inserting record: " . $conn->error;
+            }
+            
+        $conn->close();         
+        
 }
 
 ?>
